58f6ad2901988811a934739a45be1e65e052f9b0
hunchmen <valalberto0214@gmail.com>
2025-10-06 22:05:42 +0800

    section: 2 the sx prop

---
+++ mui-via/src/section2/components/SxPropComponent.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+export default function SxPropComponent() {
+  return (
+    <div className="App">
+        <h1>Hello Codesandbox</h1>
+        <h2>Start editing to see some magic</h2>
+        <Button sx={{}} variant="contained">My Button</Button>
+    </div>
+  )
+}       
