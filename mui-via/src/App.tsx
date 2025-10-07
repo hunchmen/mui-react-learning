9bce2897b3fbf83fb2f238bc53c5958df3fbfe5b
hunchmen <valalberto0214@gmail.com>
2025-10-07 20:50:01 +0800

    section2: ep14 - reusable styles

--- mui-via/src/App.tsx
+++ mui-via/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'
 import StylingBasics from './section2/components/StylingBasics';
 import SxPropComponent from './section2/components/SxPropComponent'
+import ReusableStyles from './section2/components/ReusableStyles';
 
 
 function App() {
 
   return (
     <React.Fragment>
+      <ReusableStyles />
       <StylingBasics />
       <SxPropComponent />
     </React.Fragment>
---
+++ mui-via/src/section2/components/ReusableStyles.tsx
@@ -0,0 +1,29 @@
+import { Button, Stack } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+//dynamic styling
+const DynamicStyledButton = props => {
+    return (
+        <Button sx={{color: props.color, bgcolor: props.color}} >
+            {props.label}
+        </Button>
+    );
+}
+
+//static styling
+const MyStyledButton = styled(Button)(( {theme}) => ({
+    backgroundColor: theme.palette.success.main,
+    color: "white", 
+    borderRadius: 8, 
+    padding: "8",
+}));
+
+export default function ReusableStyles() {
+    return (
+        <Stack spacing={2} direction="row">
+            <Button variant="contained">Primary</Button>
+            <MyStyledButton>My Styled Button</MyStyledButton>
+            <DynamicStyledButton label="Hello" color="purple">My Dynamic Styled Button</DynamicStyledButton>
+        </Stack>
+    )
+}
